dc40d293ffa2df6988e2e611aa0398765914fdb8
Hans-J. Schmid <knock@myopendoor.de>
2025-07-25 01:44:52 +0200

    init

---
+++ src/koans/08_conditionals.clj
@@ -0,0 +1,48 @@
+(ns koans.08-conditionals
+  (:require [koan-engine.core :refer [meditations]]))
+
+(defn explain-exercise-velocity [exercise-term]
+  (case exercise-term
+    :bicycling        "pretty fast"
+    :jogging          "not super fast"
+    :walking          "not fast at all"
+    "is that even exercise?"))
+
+(meditations
+ "You will face many decisions"
+ (= :a (if (false? (= 4 5))
+         :a
+         :b))
+
+ "Some of them leave you no alternative"
+ (= [] (when (> 4 3)
+         []))
+
+ "And in such a situation you may have nothing"
+ (= nil
+    (when (nil? 0)
+      [:a :b :c]))
+
+ "In others your alternative may be interesting"
+ (= :glory (if (seq ())
+             :doom
+             :glory))
+
+ "You may have a multitude of possible paths"
+ (let [x 5]
+   (= :your-road (cond (= x 1) :road-not-taken
+                       (= x 2) :another-road-not-taken
+                       :else :your-road)))
+
+ "Or your fate may be sealed"
+ (= 'doom (if-not (zero? 1)
+            'doom
+            'more-doom))
+
+ "In case of emergency, go fast"
+ (= "pretty fast"
+    (explain-exercise-velocity :bicycling))
+
+ "But admit it when you don't know what to do"
+ (= "is that even exercise?"
+    (explain-exercise-velocity :watching-tv)))
